f25afe1056546b45cd2995d9e7440a258da7395c
DiositoDiego <20213tn096@utez.edu.mx>
2024-02-11 23:55:45 -0600

    reto cumplido jejeje

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const db = require('./db/connection');
 const axios = require('axios');
 
+let clientNumber = 1;
+
 const createClient = () => {
     axios.get('https://randomuser.me/api/')
         .then((response) => {
@@ -9,6 +11,7 @@ const createClient = () => {
             db.query(sql, (err, result) => {
                 if (err) throw err;
                 console.log('Cliente creado!');
+                clientNumber++;
                 /*const sql = `INSERT INTO logs (description, time_stamp) VALUES ('Cliente creado', NOW())`;
                 db.query(sql, (err, result) => {
                     if (err) throw err;
@@ -21,4 +24,20 @@ const createClient = () => {
         });
 }
 
+const names = ["Max", "Bella", "Charlie", "Lucy", "Cooper", "Luna", "Rocky", "Daisy", "Buddy", "Molly"]
+const types = ["Perro", "Gato", "Conejo", "Pájaro", "Hurón", "Tortuga", "Hamster", "Cobaya", "Iguana", "Cerdo vietnamita"]
+
+
+const createPet = () => {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const type = types[Math.floor(Math.random() * types.length)];
+    const sql = `INSERT INTO pets (name, type, client_id, created_at) VALUES ('${name}','${type}', '${clientNumber}', '2024-02-11 19:07:41')`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log('Mascota creada!');
+    });
+}
+
+
 setInterval(createClient,5000);
+setInterval(createPet,6000);
